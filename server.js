fb23a41b302657f25ec340de490c4eea57133595
maxkram <max.kramer@live.com>
2017-03-10 14:12:24 +0300

    trying middleware

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log();
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
